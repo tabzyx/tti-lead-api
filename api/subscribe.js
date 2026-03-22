022a5c2fe92c59161a9398025c02100942e772e0
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-22 17:06:49 +0500

    Restrict CORS to specific origin

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // ✅ Allow CORS
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "https://ttilabs.tabzyx.com")
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
 
